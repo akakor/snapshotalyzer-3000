8f50fe48983c70a24e7a10610819497918712ef6
Denis de Montes <denis.demontes@yahoo.com>
2018-05-24 14:38:40 +0200

    Error handling command added

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,4 +1,5 @@
 import boto3
+import botocore
 import click
 
 session = boto3.Session(profile_name='shotty')
@@ -94,6 +95,7 @@ def create_snapshot(project):
         i.wait_until_running()
 
     print("Job's done!!!")
+
     return
 
 
@@ -123,8 +125,11 @@ def stop_instances(project):
 
     for i in instances:
         print("Stopping {0}...", format(i.id))
-        i.stop()
-
+        try:
+            i.stop()
+        except botocore.exceptions.ClienError as e:
+            print("Could not stop {0}. ".format(i.id) + str(e))
+            continue
     return
 
 @instances.command('start')
@@ -135,7 +140,11 @@ def stop_instances(project):
 
     for i in instances:
         print("Starting {0}...", format(i.id))
-        i.start()
+        try:
+                i.start()
+        except botocore.exceptions.ClienError as e:
+                print("Could not start {0}. ".format(i.id) + str(e))
+                continue
 
     return
 
